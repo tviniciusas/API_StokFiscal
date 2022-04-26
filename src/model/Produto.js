71d542e5658fada4008ac70dd050e26964b63ce7
tviniciusas <tv.dclxvii@gmail.com>
2022-04-26 12:47:28 -0300

    ajustando rota produtos

--- src/model/Produto.js
+++ src/model/Produto.js
@@ -4,7 +4,7 @@ module.exports = {
   async createTable(req, res) {
 
     let SQL =
-      "CREATE TABLE IF NOT EXISTS Produtos (" +
+      "CREATE TABLE IF NOT EXISTS produtos (" +
       "id int(10) NOT NULL AUTO_INCREMENT," +
       "cod_prod varchar(100) NOT NULL," +
       "descricao varchar(255) NOT NULL," +
