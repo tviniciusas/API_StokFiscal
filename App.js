054fc6d169ab6386b36abed195fe360a5d85d8ef
Thiago Vinicius <tv.dclxvii@gmail.com>
2022-07-11 19:01:33 -0300

    ajuste reader

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ var cors = require('cors');
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors());
     next();
